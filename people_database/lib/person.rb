a3b93ec7d3a552145870b7f7f32fd963e4b32659
Nick Weiland <nickweiland@gmail.com>
2016-02-16 06:53:11 -0700

    Add completed challenge in ruby

--- people_database/lib/person.rb
+++ people_database/lib/person.rb
@@ -1,3 +1,10 @@
 class Person
+  attr_reader :first_name, :last_name, :email, :state
 
+  def initialize(args)
+    @first_name = args[:first_name]
+    @last_name = args[:last_name]
+    @email = args[:email]
+    @state = args[:state]
+  end
 end
--- people_database/test/test_helper.rb
+++ people_database/test/test_helper.rb
@@ -4,3 +4,58 @@ require 'minitest/autorun'
 require 'minitest/pride'
 require 'person'
 require 'people_database'
+
+module TestData
+  def gather_data
+    person_one = {
+            first_name: "Jose",
+            last_name:  "Buendia",
+            email:      "jose@gmail.com",
+            state:      "CA"
+            }
+
+    person_two = {
+            first_name: "Aureliano",
+            last_name:  "Buendia",
+            email:      "aureliano@gmail.com",
+            state:      "CA"
+            }
+
+    person_three = {
+            first_name: "Jose",
+            last_name:  "Arcadio",
+            email:      "arcadio@gmail.com",
+            state:      "UT"
+            }
+
+    person_four = {
+            first_name: "Aureliano",
+            last_name:  "Segundo",
+            email:      "segundo@gmail.com",
+            state:      "NV"
+            }
+
+    person_five = {
+            first_name: "Ursula",
+            last_name:  "Iguaran",
+            email:      "ursula@gmail.com",
+            state:      "OR"
+            }
+
+    person_six = {
+            first_name: "Pilar",
+            last_name:  "Ternera",
+            email:      "pilar@gmail.com",
+            state:      "WA"
+            }
+
+    people_database = PeopleDatabase.new
+    people_database.add(person_one)
+    people_database.add(person_two)
+    people_database.add(person_three)
+    people_database.add(person_four)
+    people_database.add(person_five)
+    people_database.add(person_six)
+    people_database
+  end
+end

ff988dcfa6db0de3278170d61423d22787333a6d
marinacor1 <marinaspam1234@gmail.com>
2016-02-16 09:00:24 -0700

    creating initalize methods

--- people_database/ruby/lib/people_database.rb
+++ people_database/ruby/lib/people_database.rb
@@ -1,2 +1,10 @@
 class PeopleDatabase
+
+  def initialize(person)
+    array = []
+  end
+
+  def add(arguments)
+
+  end
 end
--- people_database/ruby/lib/person.rb
+++ people_database/ruby/lib/person.rb
@@ -1,3 +1,17 @@
 class Person
 
+  def initialize(first_name)
+    @first_name = first_name
+    @last_name = last_name
+    @email = email
+    @state = state
+  end
+
+  def person(arguments)
+    {first_name: @first_name, last_name: @last_name, email: @email, state: @state}
+  end
+
+
+
+
 end

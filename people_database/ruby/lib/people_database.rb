6b8cd2365f3105aba37c6f55e8f539bf72055ff5
Nicole Hall <nicolealexandrahall@gmail.com>
2016-02-17 08:58:31 -0700

    working towards completing challenge in ruby

--- people_database/ruby/lib/people_database.rb
+++ people_database/ruby/lib/people_database.rb
@@ -1,2 +1,14 @@
+require_relative 'person.rb'
+
 class PeopleDatabase
+
+  attr_reader :people
+
+  def initialize
+    @people = []
+  end
+
+  def add(person)
+    @people << Person.new(person_hash)
+  end
 end
--- people_database/ruby/lib/person.rb
+++ people_database/ruby/lib/person.rb
@@ -1,3 +1,12 @@
 class Person
 
+  attr_reader :first_name, :last_name, :email, :state
+
+  def initialize(person_hash)
+    @first_name = person_hash[:first_name]
+    @last_name = person_hash[:last_name]
+    @email = person_hash[:email]
+    @state = person_hash[:state]
+  end
+
 end

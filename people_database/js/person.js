dcf3df0ac8272c6b2d26536d716dacd728bb4455
Nick Weiland <nickweiland@gmail.com>
2016-02-16 23:44:51 -0700

    Add clojure

--- people_database/js/person.js
+++
@@ -1,8 +0,0 @@
-var Person = function(args) {
-  this.first_name = args.first_name
-  this.last_name = args.last_name
-  this.email = args.email
-  this.state = args.state
-};
-
-module.exports = Person;

a9accb4c8f295449ebdb6025ee540edf0b005753
GregoryArmstrong <intergalacticgentleman@gmail.com>
2016-02-17 09:00:46 -0700

    Added remove person by email meethod, and return all people by state method

--- people_database/ruby/lib/people_database.rb
+++ people_database/ruby/lib/people_database.rb
@@ -2,13 +2,24 @@ require_relative 'person.rb'
 require 'pry'
 
 class PeopleDatabase
-  attr_reader :people
+  attr_accessor :people
 
   def initialize
     @people = []
   end
 
   def add(data)
-    people << Person.new(data)
+    @people << Person.new(data)
   end
+
+  def remove(email)
+    @people.delete_if { |person| person.email == email }
+  end
+
+  def all_people(state)
+    chosen_people = []
+    @people.each { |person| chosen_people << person if person.state == state }
+    chosen_people
+  end
+
 end

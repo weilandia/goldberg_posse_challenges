294966a220e4828ff5440a646a6f1459d00b427c
Nick Weiland <nickweiland@gmail.com>
2016-02-23 13:39:07 -0700

    Setup structure

--- enumerables/lib/enums.rb
+++ enumerables/lib/enums.rb
@@ -1,2 +1,14 @@
 class Enums
+  def self.any?(collection, condition)
+    true
+  end
+
+  def self.select(collection, condition)
+  end
+
+  def self.reduce(collection, condition)
+  end
+
+  def self.reject(collection, condition)
+  end
 end
--- enumerables/spec/enums_spec.rb
+++ enumerables/spec/enums_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Enums do
-
-
+  it "returns true if any" do
+    collection = [1, 2, 3, 4, 5]
+    condition = Proc.new { |a| a > 6 }
+    refute Enums.any?(collection, condition)
+  end
 end

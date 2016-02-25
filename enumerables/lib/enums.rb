016172239c0bc2a6560405264ece29abe4579ee0
Nick Weiland <nickweiland@gmail.com>
2016-02-25 00:40:13 -0700

    Add reduce with extra symbol func

--- enumerables/lib/enums.rb
+++ enumerables/lib/enums.rb
@@ -7,11 +7,39 @@ class Enums
   end
 
   def self.select(collection, condition)
+    result = []
+    collection.each do |n|
+      result << n if condition.call(n)
+    end
+    result
   end
 
   def self.reduce(collection, condition)
+    if condition.class == Proc
+      reduce_with_proc(collection, condition)
+    else
+      reduce_with_symbol(collection, condition)
+    end
   end
 
   def self.reject(collection, condition)
+  end
+
+  def self.reduce_with_proc(collection, condition)
+    result = []
+    collection.each do |n|
+      result << condition.call(n)
+    end
+    result
+  end
+
+  def self.reduce_with_symbol(collection, condition)
+    result = collection[0]
+    collection.shift
+    collection.each do |n|
+      m = result.method(condition.to_s)
+      result = m.call(n)
+    end
+    result
   end
 end
--- enumerables/spec/enums_spec.rb
+++ enumerables/spec/enums_spec.rb
@@ -6,4 +6,45 @@ describe Enums do
     condition = Proc.new { |a| a > 6 }
     refute Enums.any?(collection, condition)
   end
+
+  it "returns true if there are any" do
+    collection = [1, 2, 3, 4, 5]
+    condition = Proc.new { |a| a < 2 }
+    assert Enums.any?(collection, condition)
+  end
+
+  it "returns an array of elements that meet condition" do
+    collection = [1, 2, 3, 4, 5]
+    condition = Proc.new { |a| a % 2 == 0 }
+
+    assert_equal [2,4], Enums.select(collection, condition)
+  end
+
+  it "returns an empty array if no element meets condition" do
+    collection = [1, 2, 3, 4, 5]
+    condition = Proc.new { |a| a > 5 }
+
+    assert_equal [], Enums.select(collection, condition)
+  end
+
+  it "returns an array where each element has changed based on criteria" do
+    collection = ["Rachel", "Joanne", "Nate"]
+    condition = Proc.new { |a| "#{a} works at Turing." }
+
+    assert_equal ["Rachel works at Turing.", "Joanne works at Turing.", "Nate works at Turing."], Enums.reduce(collection, condition)
+  end
+
+  it "returns an array where each element has been added together" do
+    collection = [*1..5]
+    condition = :+
+
+    assert_equal 15, Enums.reduce(collection, condition)
+  end
+
+  it "returns an array where each element has been subtracted" do
+    collection = [*1..5]
+    condition = :-
+
+    assert_equal (-13), Enums.reduce(collection, condition)
+  end
 end
